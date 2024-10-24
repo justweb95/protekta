e522dd96af35467ca747b31aad153c3e2cd0aa9b
Pavle Pesic <p.pavle16@gmail.com>
2024-10-24 23:38:32 +0200

    All statick section are done

--- assets/script/main.js
+++ assets/script/main.js
@@ -2,3 +2,20 @@ import {
     
 } from './partials/gallery.js'
 
+
+
+document.querySelectorAll('.full-name input, .email input, .number input').forEach(input => {
+  const label = input.previousElementSibling;
+
+  const toggleLabel = () => {
+      if (input.value.trim() !== '' || document.activeElement === input) {
+          label.classList.add('input-active');
+      } else {
+          label.classList.remove('input-active');
+      }
+  };
+
+  input.addEventListener('focus', toggleLabel);
+  input.addEventListener('blur', toggleLabel);
+  input.addEventListener('input', toggleLabel);
+});
