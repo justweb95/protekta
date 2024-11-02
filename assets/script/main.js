5432bc51bf069b07dcfabdf789c65bf357dbcd67
Pavle Pesic <p.pavle16@gmail.com>
2024-11-02 20:54:35 +0100

    Burger menu finish

--- assets/script/main.js
+++ assets/script/main.js
@@ -4,9 +4,17 @@ import { handleBurgerMenu } from './partials/burgerControl.js';
 // Burger Menu
 const burgerMenuOpen = document.querySelector('.burger-menu-open');
 const burgerMenuClose = document.querySelector('.burger-menu-close');
+const burgerMenuHolder = document.querySelector('.burger-menu-holder');
 
 burgerMenuOpen.addEventListener('click', handleBurgerMenu);
 burgerMenuClose.addEventListener('click', handleBurgerMenu);
+
+burgerMenuHolder.addEventListener('click', (event) => {
+    // Check if the target is an <li> or <a> element
+    if (event.target.tagName === 'LI' || event.target.tagName === 'A') {
+        handleBurgerMenu();
+    }
+});
 
 // Form function
 document.querySelectorAll('.full-name input, .email input, .number input').forEach(input => {
--- assets/script/partials/burgerControl.js
+++ assets/script/partials/burgerControl.js
@@ -1,14 +1,14 @@
-function handleBurgerMenu() {
+function handleBurgerMenu() {    
     const burgerMenu = document.getElementById('burger-menu');
-    const body = document.body;
+    const body = document.querySelector('body');
 
     burgerMenu.classList.toggle('burger-menu-active');
 
-    if (burgerMenu.classList.contains('burger-menu-active')) {
-        body.style.overflow = 'hidden';
-    } else {
-        body.style.overflow = '';
-    }
+    // if (burgerMenu.classList.contains('burger-menu-active')) {
+    //     body.style.overflow = 'hidden';
+    // } else {
+    //     body.style.overflow = '';
+    // }
     
 }
 
